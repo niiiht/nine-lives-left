1a95d4b12f52e7f5fd192dfb70916fb785fe409e
Michalina0407 <michalina0407@gmail.com>
2026-04-12 00:03:13 +0200

    push bezpieczenstwa

--- Assets/menucontroler.cs
+++ Assets/menucontroler.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class menucontroler : MonoBehaviour
 {
-    public GameObject menuCanvas;
+    public GameObject Menucanvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        menuCanvas.SetActive(false);
+        Menucanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Tab)){
-            menuCanvas.SetActive(!menuCanvas.activeSelf);
+            Menucanvas.SetActive(!Menucanvas.activeSelf);
         }
     }
 }
