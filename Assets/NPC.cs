cebdb469bf1ee03b8528a96ce1bbb7e11454af43
Paolinka22 <paula.olszewsk@gmail.com>
2026-04-11 21:13:41 +0200

    startNpc

---
+++ Assets/NPC.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public NPCDialouge dialougeData;
+    public GameObject dialougePanel;
+    public TMP_Text dialougeText, nameText;
+    public Image portraitImage;
+
+    private int dialougeIndex;
+    private bool isTyping, isDialougeActive;
+
+    public bool CanInteract()
+    {
+        return !isDialougeActive;
+    }
+    public void Interact()
+    {
+        if (dialougeData == null/* || (PauseController.IsGamePaused && !isDialougeActive (nie mamy pauzyzaimplementowanej))*/)
+            return;
+        if (isDialougeActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialouge();
+        }
+    }
+    void StartDialouge()
+    {
+        isDialougeActive = true;
+        dialougeIndex = 0;
+
+        nameText.SetText(dialougeData.npcName);
+        portraitImage.sprite = dialougeData.npcPortrait;
+
+        dialougePanel.SetActive(true);
+        //PauseController.SetPause(true);
+
+        StartCoroutine(TypeLine());
+
+    }
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialougeText.SetText(dialougeData.dialougeLines[dialougeIndex]);
+            isTyping = false;
+        }
+        else if (++dialougeIndex < dialougeData.dialougeLines.Length)
+        {
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialouge();
+        }
+    }
+            IEnumerator TypeLine()
+            {
+                isTyping = true;
+                dialougeText.SetText("");
+
+                foreach (char letter in dialougeData.dialougeLines[dialougeIndex])
+                {
+                    dialougeText.text += letter;
+                    yield return new WaitForSeconds(dialougeData.typingSpeed);
+                }
+                isTyping = false;
+                if (dialougeData.autoProgressLines.Length > dialougeIndex && dialougeData.autoProgressLines[dialougeIndex])
+                {
+                    yield return new WaitForSeconds(dialougeData.autoProgerssDelay);
+                    NextLine();
+                }
+            }
+    public void EndDialouge()
+    {
+        StopAllCoroutines();
+        isDialougeActive = false;
+        dialougeText.SetText("");
+        dialougePanel.SetActive(false);
+        //PauseController.SetPause(fals+e);
+    }
+}
+
