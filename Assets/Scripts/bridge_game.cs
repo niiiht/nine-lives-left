d70484f1dba7b3aec59e1122e411d76889271845
niiiht <natkamb@o2.pl>
2026-04-28 02:35:56 +0200

    commmit

--- Assets/Scripts/bridge_game.cs
+++ Assets/Scripts/bridge_game.cs
@@ -1,41 +1,116 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 public class bridge_game : MonoBehaviour
 {
+    public RectTransform pasek;
+    public RectTransform greenZone;
+
+    // public Image myImage;
+    // public Sprite[] jumpSprites ;
+
     public GameObject[] states;
+  
+    public GameObject yellow_pasek;
     private int current = 0;
 
-    public GameObject pasek; // assign in Inspector
     private Animator pasekAnimator;
+
+    private bool isPaused = false;
+    private Image jumpBar;
+    private GameObject greenBar1;
+    private Sprite barChange1;
+    private Sprite barChange2;
+    private GameObject secondBar;
+
+
+
 
     void Start()
     {
-        ShowState(0);
+        pasekAnimator = yellow_pasek.GetComponent<Animator>();
+        jumpBar = GameObject.Find("jump_bar").GetComponent<Image>();
+        greenBar1 = GameObject.Find("pasek_zielony1");
+        barChange1 = Resources.Load<Sprite>("pasek2");
+       
 
-        // get animator once
-        pasekAnimator = pasek.GetComponent<Animator>();
     }
 
     void Update()
     {
+        if (isPaused) return;
+
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("click");
-
-            if (pasekAnimator != null)
+            if (IsInGreen())
             {
-                pasekAnimator.speed = 0;
+                Debug.Log("SUCCESS");
+                // for(int i = 0 ; i< jumpSprites.Length; i++)
+                // {
+                //     myImage.sprite = jumpSprites[i];
+                    
+                // }
+                // //   myImage.sprite = newSprite;
+                // StartCoroutine(PauseAndContinue());
+                 NextState();
             }
-
-            NextState(); // move to next rock
+            else
+            {
+                Debug.Log("FAIL");
+            }
         }
     }
+
+    bool IsInGreen()
+    {
+        Vector3[] corners = new Vector3[4];
+        greenZone.GetWorldCorners(corners);
+
+        float left = corners[0].x;
+        float right = corners[3].x;
+
+        float yellowX = pasek.position.x;
+
+        return yellowX >= left && yellowX <= right;
+    }
+
+    // IEnumerator PauseAndContinue()
+    // {
+    //     isPaused = true;
+
+    //     // stop animation
+    //     pasekAnimator.speed = 0;
+
+    //     // wait 2 seconds (real time)
+    //     yield return new WaitForSeconds(0.1f);
+
+    //     NextState();
+
+    //     // resume animation
+    //     pasekAnimator.speed = 1;
+
+    //     isPaused = false;
+    // }
 
     void NextState()
     {
         if (current < states.Length - 1)
         {
             current++;
+            if(current == 1)
+            {
+                  jumpBar.sprite = barChange1;
+                  greenBar1.SetActive(false);
+                  
+
+               
+            
+            }else if(current == 2){
+                
+                
+            }
+          
             ShowState(current);
         }
     }
@@ -44,6 +119,7 @@ public class bridge_game : MonoBehaviour
     {
         for (int i = 0; i < states.Length; i++)
         {
+    
             states[i].SetActive(i == index);
         }
     }
